a6c5008c28c1b02a55ee4056fe96c96e60e2cc71
Ivan Vermeyen <ivan@vermeyen.org>
2019-11-29 13:41:29 +0100

    Install frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,13 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('mix-tailwindcss');
+require('laravel-mix-purgecss');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
+mix.extract(['vue']);
+
+mix.sass('resources/sass/app.scss', 'public/css');
+mix.tailwind();
+mix.purgeCss();
+
+mix.version();
