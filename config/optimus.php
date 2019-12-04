b3fcb6aef83f8b9f12303bd6a6a6ea49ea2becc3
Ivan Vermeyen <ivan@vermeyen.org>
2019-12-04 14:58:14 +0100

    Install laravel-optimus

---
+++ config/optimus.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of Laravel Optimus.
+ *
+ * (c) Anton Komarev <anton@komarev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Optimus Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'prime' => 407170613,
+            'inverse' => 1868395549,
+            'random' => 2087260747,
+        ],
+
+    ],
+
+];
