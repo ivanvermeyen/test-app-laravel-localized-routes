b43350db26fbf6c3c0ac40a7268f9acc95112858
Ivan Vermeyen <ivan@vermeyen.org>
2019-12-04 16:36:22 +0100

    Install laravel-localized-routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -76,6 +76,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\Authenticate::class,
         \Illuminate\Routing\Middleware\ThrottleRequests::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \CodeZero\LocalizedRoutes\Middleware\LocalizedRouteLocaleHandler::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
     ];
---
+++ config/localized-routes.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /**
+     * The locales you wish to support.
+     */
+    'supported-locales' => ['en', 'nl'],
+
+    /**
+     * If you have a main locale and don't want
+     * to prefix it in the URL, specify it here.
+     *
+     * 'omit_url_prefix_for_locale' => 'en',
+     */
+    'omit_url_prefix_for_locale' => null,
+
+    /**
+     * If you want to automatically set the locale
+     * for localized routes set this to true.
+     */
+    'use_locale_middleware' => true,
+
+];
